714ef50031a89449982ca7da971f8d82ea9b3940
JunMoLee <59989788+JunMoLee@users.noreply.github.com>
2020-01-29 18:57:52 +0900

    Update Cell.cpp

--- Cell.cpp
+++ Cell.cpp
@@ -334,18 +334,18 @@ RealDevice::RealDevice(int x, int y, double p, double n) {
 
 
 	NL_LTP_Gp=p;
-	NL_LTN_Gp=0;
+	NL_LTD_Gp=0;
 	NL_LTP_Gn=n;
-	NL_LTN_Gn=0;
+	NL_LTD_Gn=0;
 
 	sigmaDtoD = 0;	// Sigma of device-to-device weight update vairation in gaussian distribution
 	gaussian_dist2 = new std::normal_distribution<double>(0, sigmaDtoD);	// Set up mean and stddev for device-to-device weight update vairation
 	paramALTP = getParamA(NL_LTP + (*gaussian_dist2)(localGen)) * maxNumLevelLTP;	// Parameter A for LTP nonlinearity
 	paramALTD = getParamA(NL_LTD + (*gaussian_dist2)(localGen)) * maxNumLevelLTD;	// Parameter A for LTD nonlinearity
 	paramAGp= getParamA(NL_LTP_Gp + (*gaussian_dist2)(localGen)) * maxNumLevelLTP;
-	paramAGpd= getParamA(NL_LTN_Gp + (*gaussian_dist2)(localGen)) * maxNumLevelLTP;
+	paramAGpd= getParamA(NL_LTD_Gp + (*gaussian_dist2)(localGen)) * maxNumLevelLTP;
 	paramAGn= getParamA(NL_LTP_Gn + (*gaussian_dist2)(localGen)) * maxNumLevelLTP;
-	paramAGnd= getParamA(NL_LTN_Gn + (*gaussian_dist2)(localGen)) * maxNumLevelLTP;
+	paramAGnd= getParamA(NL_LTD_Gn + (*gaussian_dist2)(localGen)) * maxNumLevelLTP;
 	paramBGp= (pmaxConductance - pminConductance) / (1 - exp(-maxNumLevelpLTP/paramAGp));
 	paramBGpd= (pmaxConductance - pminConductance) / (1 - exp(-maxNumLevelpLTD/paramAGpd));
 	paramBGn= (nmaxConductance - nminConductance) / (1 - exp(-maxNumLevelnLTP/paramAGn));;
