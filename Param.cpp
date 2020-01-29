99af98f74bd552295518803897b30d867e2fca2d
JunMoLee <59989788+JunMoLee@users.noreply.github.com>
2020-01-29 15:35:06 +0900

    Update Param.cpp

--- Param.cpp
+++ Param.cpp
@@ -60,7 +60,8 @@ Param::Param() {
     /*Optimization method 
     Available option include: "SGD", "Momentum", "Adagrad", "RMSprop" and "Adam"*/
     optimization_type = "SGD";
-
+        ReverseUpdate = 1;
+	FullRefresh=0;
 
 	/* Hardware parameters */
 	useHardwareInTrainingFF = true;   // Use hardware in the feed forward part of training or not (true: realistic hardware, false: ideal software)
