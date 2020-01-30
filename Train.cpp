337b1747e66d9d5c779d62a6908f9bde32911646
JunMoLee <59989788+JunMoLee@users.noreply.github.com>
2020-01-30 16:49:56 +0900

    Update Train.cpp

--- Train.cpp
+++ Train.cpp
@@ -540,10 +540,10 @@ double s2[param->nOutput];  // Output delta from hidden layer to the output laye
                             
 							if (AnalogNVM *temp = dynamic_cast<AnalogNVM*>(arrayIH->cell[jj][k])) {	// Analog eNVM
 								if((batchSize % param->newUpdateRate)*param->ReverseUpdate==(param->newUpdateRate)-1){
-							        arrayIH->WriteCell(jj, k, deltaWeight1[jj][k], weight1[jj][k], param->maxWeight, param->minWeight, true);
+							        arrayIH->WriteCell(jj, k, deltaWeight1[jj][k], weight1[jj][k], param->maxWeight, param->minWeight, true, true);
 								}
 								else{
-								arrayIH->WriteCell(jj, k, deltaWeight1[jj][k], weight1[jj][k], param->maxWeight, param->minWeight, true);
+								arrayIH->WriteCell(jj, k, deltaWeight1[jj][k], weight1[jj][k], param->maxWeight, param->minWeight, true, false);
 								}
 								
 								
@@ -857,11 +857,11 @@ double s2[param->nOutput];  // Output delta from hidden layer to the output laye
 							if (AnalogNVM *temp = dynamic_cast<AnalogNVM*>(arrayHO->cell[jj][k])) { // Analog eNVM
 								if((batchSize % param->newUpdateRate)*param->ReverseUpdate==(param->newUpdateRate)-1){
 									
-                                                                arrayHO->WriteCell(jj, k, deltaWeight2[jj][k], weight2[jj][k], param->maxWeight, param->minWeight, true);}
+                                                                arrayHO->WriteCell(jj, k, deltaWeight2[jj][k], weight2[jj][k], param->maxWeight, param->minWeight, true, true);}
 								
 								else{
 									
-                                                                arrayHO->WriteCell(jj, k, deltaWeight2[jj][k], weight2[jj][k], param->maxWeight, param->minWeight, true);}
+                                                                arrayHO->WriteCell(jj, k, deltaWeight2[jj][k], weight2[jj][k], param->maxWeight, param->minWeight, true , false);}
 								
 								
 						                weight2[jj][k] = arrayHO->ConductanceToWeight(jj, k, param->maxWeight, param->minWeight);
