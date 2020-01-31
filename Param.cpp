83c2f3926b3b0d10812b4138de066aaae6462f6d
JunMoLee <59989788+JunMoLee@users.noreply.github.com>
2020-01-31 16:17:06 +0900

    Update Param.cpp

--- Param.cpp
+++ Param.cpp
@@ -93,77 +93,45 @@ const int
 a=1;
 	
 switch(a){
-case 1:
+case 0:
 param_gp=0;
 param_gn=0;
+break;
+case 1:
+param_gp=1;
+param_gn=-1;
 break;
 case 2:
 param_gp=1;
-param_gn=0;
+param_gn=-3;
 break;
 case 3:
 param_gp=1;
-param_gn=1;
+param_gn=-5;
 break;
 case 4:
-param_gp=0;
-param_gn=2;
+param_gp=3;
+param_gn=-1;
 break;
 case 5:
-param_gp=2;
-param_gn=0;
+param_gp=3;
+param_gn=-3;
 break;
 case 6:
 param_gp=3;
-param_gn=0;
+param_gn=-5;
 break;
 case 7:
-param_gp=0;
-param_gn=3;
+param_gp=5;
+param_gn=-1;
 break;
 case 8:
-param_gp=1;
-param_gn=4;
+param_gp=5;
+param_gn=-3;
 break;
 case 9:
-param_gp=4;
-param_gn=1;
-break;
-case 10:
-param_gp=2;
-param_gn=6;
-break;
-case 11:
-param_gp=6;
-param_gn=2;
-break;
-case 12:
-param_gp=2;
-param_gn=2;
-break;
-case 13:
-param_gp=4;
-param_gn=4;
-break;
-case 14:
-param_gp=3;
-param_gn=5;
-break;
-case 15:
 param_gp=5;
-param_gn=3;
-break;
-case 16:
-param_gp=4;
-param_gn=7;
-break;
-case 17:
-param_gp=7;
-param_gn=4;
-break;
-case 18:
-param_gp=3;
-param_gn=3;
+param_gn=-5;
 break;
 }
 	
