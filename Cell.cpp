2abbaebec4dae050acec6a85ab66b353a551a3bb
JunMoLee <59989788+JunMoLee@users.noreply.github.com>
2020-01-31 16:18:38 +0900

    Update Cell.cpp

--- Cell.cpp
+++ Cell.cpp
@@ -345,7 +345,7 @@ RealDevice::RealDevice(int x, int y, double p, double n) {
 	NL_LTP = 2.4;	// LTP nonlinearity
 	NL_LTD = -4.88;	// LTD nonlinearity
 
-        (void)p; (void)n;
+   
 	const double
 		pp=1;
 	const double
@@ -354,14 +354,14 @@ RealDevice::RealDevice(int x, int y, double p, double n) {
 		cp=1;
 	const double
 		cn=-1;
-	NL_LTP_Gp=cp;
-	NL_LTD_Gp=cn;
+	NL_LTP_Gp=p;
+	NL_LTD_Gp=n;
 	const double
 		np=1;
 	const double
 		nd=-1;
-	NL_LTP_Gn=cp;
-	NL_LTD_Gn=cn;
+	NL_LTP_Gn=p;
+	NL_LTD_Gn=n;
 
 	sigmaDtoD = 0;	// Sigma of device-to-device weight update vairation in gaussian distribution
 	gaussian_dist2 = new std::normal_distribution<double>(0, sigmaDtoD);	// Set up mean and stddev for device-to-device weight update vairation
