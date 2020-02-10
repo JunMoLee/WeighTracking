57b3c014dadfdd79fb86086ea4fa8789fddf6614
JunMoLee <59989788+JunMoLee@users.noreply.github.com>
2020-02-10 15:49:42 +0900

    Update Param.cpp

--- Param.cpp
+++ Param.cpp
@@ -104,40 +104,24 @@ param_gp=1;
 param_gn=2;
 break;
 case 2:
-param_gp=1;
-param_gn=3;
-break;
-case 3:
-param_gp=1;
+param_gp=2;
 param_gn=4;
 break;
+case 3:
+param_gp=4;
+param_gn=3;
+break;
 case 4:
-param_gp=1;
-param_gn=5;
-break;
-case 5:
-param_gp=1;
-param_gn=6;
-break;
-case 6:
-param_gp=2;
-param_gn=1;
-break;
-case 7:
 param_gp=3;
 param_gn=1;
 break;
-case 8:
-param_gp=4;
-param_gn=1;
-break;
-case 9:
-param_gp=5;
-param_gn=1;
-break;
-case 10:
+case 5:
 param_gp=6;
-param_gn=1;
+param_gn=3;
+break;
+case 6:
+param_gp=8;
+param_gn=4;
 break;
 
 }
