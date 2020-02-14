1c150984d019a14968fff53857354f6d49065463
JunMoLee <59989788+JunMoLee@users.noreply.github.com>
2020-02-14 22:16:59 +0900

    Update Param.cpp

--- Param.cpp
+++ Param.cpp
@@ -97,29 +97,29 @@ a=0;
 if(true) {
 switch(a){
 case 0:
-param_gp=0;
-param_gn=0;
+param_gp=1;
+param_gn=5;
 break;
 case 1:
-param_gp=1;
-param_gn=1;
+param_gp=4;
+param_gn=2;
 case 2:
-param_gp=3;
+param_gp=9;
 param_gn=3;
 break;
 case 3:
-param_gp=1;
-param_gn=5;
+param_gp=3;
+param_gn=9;
 case 4:
-param_gp=5;
-param_gn=1;
+param_gp=8;
+param_gn=9;
 break;
 case 5:
-param_gp=3;
-param_gn=6;
+param_gp=4;
+param_gn=2;
 case 6:
-param_gp=6;
-param_gn=3;
+param_gp=9;
+param_gn=8;
 break;
 case 7:
 param_gp=8;
