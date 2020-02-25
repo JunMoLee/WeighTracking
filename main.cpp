d17d5cc59611a5fefd5371051030a6f3e6802a4a
JunMoLee <59989788+JunMoLee@users.noreply.github.com>
2020-02-25 15:54:22 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -157,6 +157,7 @@ int main() {
 		
 		}
 		printf("%.2f\n", (double)correct/param->numMnistTestImages*100);
+	        SaturationCount();
 		/*printf("\tRead latency=%.4e s\n", subArrayIH->readLatency + subArrayHO->readLatency);
 		printf("\tWrite latency=%.4e s\n", subArrayIH->writeLatency + subArrayHO->writeLatency);
 		printf("\tRead energy=%.4e J\n", arrayIH->readEnergy + subArrayIH->readDynamicEnergy + arrayHO->readEnergy + subArrayHO->readDynamicEnergy);
